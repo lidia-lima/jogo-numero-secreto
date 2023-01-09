2b9651b7511621713b0d1e6b56f5616c386df63c
Lidia <115185413+lidia-lima@users.noreply.github.com>
2023-01-09 15:57:49 -0300

    Atualização: tela de game over

--- app/validacao.js
+++ app/validacao.js
@@ -2,8 +2,19 @@ function verificaSeOChutePossuiUmValorValido() {
     const numero = +chute
 
     if (chuteForInvalido(numero)) {
-        elementoChute.innerHTML += '<div>Valor invalido</div>'
-        return
+        if (chute.toUpperCase() === "GAME OVER") {
+
+            document.body.innerHTML =
+                `
+                <h2>Game Over!!!</h2>
+                <h3>Pressione o botão para jogar novamente</h3>
+                <button id="jogar-novamente" class="btn-jogar" >Jogar novamente</button>
+                `
+                document.body.style.backgroundColor = "black";
+        } else {
+
+            elementoChute.innerHTML += '<div>Valor Inválido</div>';
+        }
     }
 
     if (numeroForMaiorOuMenorQueOValorPermitido(numero)) {
@@ -33,6 +44,8 @@ function verificaSeOChutePossuiUmValorValido() {
 
 function chuteForInvalido(numero) {
     return Number.isNaN(numero)
+
+
 }
 
 function numeroForMaiorOuMenorQueOValorPermitido(numero) {
